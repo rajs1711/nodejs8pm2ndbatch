a19da0fe98c733f2fad84f3140f64a60ee76c0de
rajs1711 <rajsaurav1711@gmail.com>
2025-11-04 20:52:14 +0530

    ratelimit and exception handling

--- controller/authController.js
+++ controller/authController.js
@@ -1,33 +1,35 @@
 
 const signupFunction= async (req,res)=>{
      
+       // exception handling 
+       try{
+        // fetch user data
+        const { mobile,name ,email ,address}=req.body
+        // verify weather given email,mobile already exists in your database or not 
+        // if email and mobile already exists it means user try to duplicate
+        // that is not allowed
+        // Beacuse one email and mobile can create single account
+        // generate new password
+        const rand_num = Math.floor(10000 + Math.random() * 90000);
+        const newpassword= rand_num +'$pwd';
 
-       // fetch user data
-       const { mobile,name ,email ,address}=req.body
-       
-       // verify weather given email,mobile already exists in your database or not 
-       // if email and mobile already exists it means user try to duplicate
-       // that is not allowed
-       // Beacuse one email and mobile can create single account
 
+        // encrypt your password
+        // save your above data in database
+        // return response 
 
-       // generate new password
-       const rand_num = Math.floor(10000 + Math.random() * 90000);
-       const newpassword= rand_num +'$pwd';
+        // data fetch from your request body
+        const yourDetails={
+            "mobile":req.body.mobile,
+            "name":req.body.name,
+            "username":"user1"+req.body.mobile,
+            "temppassword":newpassword
 
-       // encrypt your password
-       // save your above data in database
-       // return response 
-
-       // data fetch from your request body
-       const yourDetails={
-        "mobile":req.body.mobile,
-        "name":req.body.name,
-        "username":"user1"+req.body.mobile,
-        "temppassword":"90ehjdud"
-
-       }
-       res.json({"msg":"your account created successfully","data":yourDetails});
+        }
+        res.json({"msg":"your account created successfully","data":yourDetails});
+    }catch(err){
+        res .json({"msg":"Internal Server Error","status":500,"err":err})
+    }
 }
 
 module.exports={
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express=require('express'); // this way we require package in our file
 const  utility =require('./utils');
 const { ValidationError } = require('express-validation');
+const { rateLimit } = require('express-rate-limit');
+
 //import express from 'express';
 //import {getData,addData} from './utils.js';
 
@@ -39,7 +41,17 @@ app.use() is used to register middleware in your Express app.
 Middleware functions are run in order before the request reaches your route handler (or after, for error-handling middleware).
 
 */
+const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000, // 1 minutes
+	limit: 5, // Limit each IP to 5 requests per `window` (here, per 5 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+	// store: ... , // Redis, Memcached, etc. See below.
+})
 
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
 app.use('/api/v1',require('./routes/commonRoutes'));
 app.use('/api/v1',require('./routes/authRoutes'));
 
